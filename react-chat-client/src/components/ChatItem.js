cae6165eb16945515282adc679f22b5a8adcaf8e
Rofi Syahrul <srofi44@gmail.com>
2019-10-18 08:27:40 +0700

    Added 'delete button' handler and used ReactMarkdown to render markdown on senders and messages

--- react-chat-client/src/components/ChatItem.js
+++ react-chat-client/src/components/ChatItem.js
@@ -1,34 +1,41 @@
 import React from "react";
+import ReactMarkdown from "react-markdown";
 import "./ReactChat.css";
 
 class ChatItem extends React.Component {
   render() {
+    const removeChat = e => {
+      e.preventDefault();
+      const id = e.target.parentElement.getAttribute("id");
+      this.props.remove(id.slice(6));
+    };
+
     return (
-      <li className="list-group-item borderless bg-transparent">
-        <div className="row justify-content-between">
-          <div
-            className="col-1 text-center"
-            style={{ verticalAlign: "middle" }}
-          >
-            <a
-              className={this.props.color}
+      <li className="list-group-item borderless bg-transparent mx-n2 mx-sm-0">
+        <div className="row justify-content-between align-items-center">
+          <div className="col-1 mr-3 mr-sm-0">
+            <button
+              type="button"
+              className={`${this.props.color} btn`}
               id={`delete${this.props.chat._id}`}
-              href="/"
+              onClick={removeChat}
             >
               <i className="fa fa-minus-circle fa-3x"></i>
-            </a>
+            </button>
           </div>
-          <div className="col-9 col-sm-11 border bg-white">
+          <div className="col-9 col-sm-11 ml-3 ml-sm-0 speech-bubble">
             <div className="row justify-content-between sender mt-2">
-              <div className={`col-auto ${this.props.color}`}>
-                {this.props.chat.sender}
+              <div className="col-auto">
+                <ReactMarkdown source={this.props.chat.sender} className={this.props.color} />
               </div>
               <div className="col-auto text-secondary">
                 {this.props.chat.time}
               </div>
             </div>
             <div className="row mb-1">
-              <div className="col-12">{this.props.chat.message}</div>
+              <div className="col-12 chat-message">
+                <ReactMarkdown source={this.props.chat.message} />
+              </div>
             </div>
           </div>
         </div>
