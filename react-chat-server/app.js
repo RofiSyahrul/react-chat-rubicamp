077314f6e3a332cdb4078ad143b7601ec1b834d6
Rofi Syahrul <srofi44@gmail.com>
2019-10-22 20:22:43 +0700

    Moved socket io to ./bin/www

--- react-chat-server/app.js
+++ react-chat-server/app.js
@@ -19,9 +19,6 @@ mongoose
   });
 
 const app = express();
-let server = require("http").Server(app);
-server = server.listen(8000);
-const io = require("socket.io")(server);
 
 app.use(logger("dev"));
 app.use(express.json());
@@ -37,19 +34,5 @@ app.use(cors());
 
 const chatRouter = require("./routes/chat");
 app.use("/api/chat", chatRouter);
-
-io.on("connection", socket => {
-  socket.on("new chat", (chatId, sender, message) => {
-    io.emit("new chat", chatId, sender, message);
-  });
-
-  socket.on("delete chat", id => {
-    io.emit("delete chat", id);
-  });
-
-  socket.on("key up", typer => {
-    socket.broadcast.emit("key up", typer);
-  });
-});
 
 module.exports = app;
